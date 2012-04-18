24d04a9a279c98ee4ac2b31ebc3b1336864f4881
alexmonteiro <alexmonteiro@gmail.com>
2012-04-18 18:34:15 -0300

    include thick box to show the issues in a modal.
    This change ill improve the exhibition of issues project showing in a
    modal thickbox.js windows. First implementation only do it on overdue
    issues.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect '/home_monitoring_controlling_project/index/:id', :controller => 'home_monitoring_controlling_project', :action => 'index'
+  map.connect '/home_monitoring_controlling_project/teste/:id', :controller => 'home_monitoring_controlling_project', :action => 'teste'
 end
