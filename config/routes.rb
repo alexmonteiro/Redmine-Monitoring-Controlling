77f205dcc29e1f67499e98b54b7d3d3eb8079c00
alexmonteiro <alexmonteiro@gmail.com>
2012-05-28 12:36:25 -0300

    Rails version check on routers and _contentHeader to works with redmine 1.4.x and 2.0.x.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-# Uncomment these files to work with Rails 2.x (i.e Redmine 1.x)
-# ActionController::Routing::Routes.draw do |map|
-#  map.connect '/home_monitoring_controlling_project/index/:id', :controller => 'home_monitoring_controlling_project', :action => 'index'
-#  map.connect '/mc_time_mgmt_project/index/:id', :controller => 'mc_time_mgmt_project', :action => 'index'
-#  map.connect '/mc_human_resource_project/index/:id', :controller => 'mc_human_resource_mgmt_project', :action => 'index'
-# end
-
-# Comment these files to work with Rails 2.x (i.e. Redmine 1.x)
+if Rails.version.to_f >= 3.0
   match 'home_monitoring_controlling_project/index/:id', :to => 'home_monitoring_controlling_project#index', :via => :get
   match 'mc_time_mgmt_project/index/:id', :to => 'mc_time_mgmt_project#index', :via => :get
   match 'mc_human_resource_project/index/:id', :to => 'mc_human_resource_mgmt_project#index', :via => :get
+else # Routes for older Rails routes
+  ActionController::Routing::Routes.draw do |map|
+   map.connect '/home_monitoring_controlling_project/index/:id', :controller => 'home_monitoring_controlling_project', :action => 'index'
+   map.connect '/mc_time_mgmt_project/index/:id', :controller => 'mc_time_mgmt_project', :action => 'index'
+   map.connect '/mc_human_resource_project/index/:id', :controller => 'mc_human_resource_mgmt_project', :action => 'index'
+  end
+end
