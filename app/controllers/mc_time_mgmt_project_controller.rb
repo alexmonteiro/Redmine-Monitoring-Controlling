9c65f5fdc89be8f55322fedde8ac10ce92615518
alexmonteiro <alexmonteiro@gmail.com>
2012-05-30 18:07:02 -0300

    Set sum estimated hours to bring only from root tasks.
    Redmine sum the estimated hours of childs tasks and update the main
    task with sum, so the query with sum estimated ours will work with only
    the main task.

--- app/controllers/mc_time_mgmt_project_controller.rb
+++ app/controllers/mc_time_mgmt_project_controller.rb
@@ -27,18 +27,15 @@ class McTimeMgmtProjectController < ApplicationController
     stringSqlProjectsSubPorjects = return_ids(@project.id).inspect.gsub("[","").gsub("]","")    
 
 
-    @issuesSpentHours = Issue.find_by_sql("select issues.due_date, sum(issues.estimated_hours) as estimated_hours, 
-                                                                 (select sum(i.estimated_hours) 
-                                                                  from issues i 
-                                                                  where i.project_id in (#{stringSqlProjectsSubPorjects})
-                                                                  and i.due_date is not null 
-                                                                  and i.due_date <= issues.due_date) as sumestimatedhours,
-                                                                  (select sum(hours) from time_entries where project_id in (#{stringSqlProjectsSubPorjects}) and created_on <= issues.due_date ) as sumspenthours
-                                         from issues 
-                                         where issues.project_id in (#{stringSqlProjectsSubPorjects})
-                                         and due_date is not null   
-                                         group by issues.due_date  
-                                        order by due_date;")    
+    @issuesSpentHours = Issue.find_by_sql("select issues.due_date, sum(issues.estimated_hours) as sumestimatedhours,
+                                           (select sum(hours) from time_entries where project_id in (#{stringSqlProjectsSubPorjects}) and created_on <= issues.due_date ) as sumspenthours
+                                           from issues 
+                                           where issues.project_id in (#{stringSqlProjectsSubPorjects})
+                                           and due_date is not null
+                                           and parent_id is null
+                                           and due_date <= issues.due_date
+                                           group by issues.due_date 
+                                           order by due_date;")    
                                         
 
     @spentHoursByVersion = Issue.find_by_sql("select versions.name as version, versions.effective_date, sum(issues.estimated_hours) as estimated_hours, 
@@ -46,17 +43,20 @@ class McTimeMgmtProjectController < ApplicationController
                                               from issues i
                                               where i.project_id in (#{stringSqlProjectsSubPorjects})
                                               and i.fixed_version_id = versions.id
-                                              and i.due_date is not null 
+                                              and i.due_date is not null
+                                              and i.parent_id is null 
                                               and i.due_date <= versions.effective_date) as sumestimatedhours,
                                              (select sum(hours) 
                                               from issues i, time_entries t
                                               where i.project_id in (#{stringSqlProjectsSubPorjects})
                                               and i.project_id = t.project_id
+                                              and i.parent_id is null
                                               and i.id = t.issue_id
                                               and i.fixed_version_id = versions.id
                                               and t.created_on <= versions.effective_date) as sumspenthours
                                              from issues, versions 
                                              where issues.project_id in (#{stringSqlProjectsSubPorjects})
+                                             and issues.parent_id is null
                                              and issues.fixed_version_id = versions.id
                                              and due_date <= versions.effective_date
                                              group by versions.id, versions.name, versions.effective_date
