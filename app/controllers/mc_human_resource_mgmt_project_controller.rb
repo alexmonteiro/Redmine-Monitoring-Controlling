526262276b484d41c5299fc62860b471f186cd0c
alexmonteiro <alexmonteiro@gmail.com>
2012-06-18 17:47:12 -0300

    Add highstock.js to implement scrollbar in Human Mgtm
    1. Include highstock.js to implement scroobar at Human Mgtm.
    2. Fixed en.yml label

--- app/controllers/mc_human_resource_mgmt_project_controller.rb
+++ app/controllers/mc_human_resource_mgmt_project_controller.rb
@@ -48,8 +48,6 @@ class McHumanResourceMgmtProjectController < ApplicationController
                                              where project_id in (#{stringSqlProjectsSubPorjects}) 
                                              group by assigned_to_id, assigned_name, issue_statuses.id, issue_statuses.name
                                              order by 2,3;")  
-    
-    
   end
 
   private
